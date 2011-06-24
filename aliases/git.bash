8f1f417269d97286832416856bd01e478f27abed
Bruno Gama <bruno@imgnation.com.br>
2011-06-24 15:38:14 -0300

    Minor updates: added PYTHONSTARTUP to the enviroment variables to enable autocomplete in the python shell

---
+++ aliases/git.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+alias	gs="git status"
+alias	gc="git commit"
+alias	gi="git init"
+alias	ga="git add"
--- env/globals.bash
+++ env/globals.bash
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+export MACOSX_DEPLOYMENT_TARGET=10.6
+export ARCHFLAGS="-arch i386 -arch x86_64"
+export PYTHONPATH
+export LC_CTYPE="pt_BR.UTF-8"
+export GREP_OPTIONS="--exclude=\*.svn\*"
+export LANG="en_US.UTF-8"
+# export LC_CTYPE="en_US.UTF-8"
+export EDITOR='mate -w'  # OS-X SPECIFIC - TextMate, w is to wait for TextMate window to close
+export HISTCONTROL=erasedups # Erase duplicates
+export HISTSIZE=15000 # resize history size
+
+export PYTHONSTARTUP="${HOME}/.pythonrc"
+
+# PATH
 [ -d "/opt/local/bin" ] && PATH="/opt/local/bin":${PATH}
 [ -d "/opt/local/sbin" ] && PATH="/opt/local/sbin":${PATH}
 [ -d "/usr/local/bin/gitx" ] && PATH="/usr/local/bin/gitx":${PATH}
 [ -d "/Library/Frameworks/Python.framework/Versions/2.7" ] && PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin:${PATH}" #set Python 2.7
-[ -d "{$HOME}/Projects/Python/xml2json" ] && PATH="{$HOME}/Projects/Python/xml2json:${PATH}"
+[ -d "${HOME}/Projects/Python/xml2json" ] && PATH="{$HOME}/Projects/Python/xml2json:${PATH}"
 [ -d "/usr/local/git" ] && PATH="/usr/local/git/bin:${PATH}"
 [ -d "${HOME}/bin" ] && PATH="${HOME}/bin:${PATH}"
 [ -d "/Library/Frameworks/Python.framework/Versions/2.7" ] && PYTHONPATH="/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/:${PYTHONPATH}"
 [ -d "/Library/Java/Home" ] && export JAVA_HOME="/Library/Java/Home"
 
-
 export PATH
-export MACOSX_DEPLOYMENT_TARGET=10.6
-export ARCHFLAGS="-arch i386 -arch x86_64"
-export PYTHONPATH
-export LC_CTYPE=pt_BR.UTF-8

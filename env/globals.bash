c872a240bbe391fe90af618c5337e61813828d4d
Bruno Gama <bruno@imgnation.com.br>
2011-06-24 16:39:10 -0300

    Added variables to the macports

--- env/globals.bash
+++ env/globals.bash
@@ -14,14 +14,16 @@ export HISTSIZE=15000 # resize history size
 export PYTHONSTARTUP="${HOME}/.pythonrc"
 
 # PATH
-[ -d "/opt/local/bin" ] && PATH="/opt/local/bin":${PATH}
-[ -d "/opt/local/sbin" ] && PATH="/opt/local/sbin":${PATH}
-[ -d "/usr/local/bin/gitx" ] && PATH="/usr/local/bin/gitx":${PATH}
-[ -d "/Library/Frameworks/Python.framework/Versions/2.7" ] && PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin:${PATH}" #set Python 2.7
-[ -d "${HOME}/Projects/Python/xml2json" ] && PATH="{$HOME}/Projects/Python/xml2json:${PATH}"
-[ -d "/usr/local/git" ] && PATH="/usr/local/git/bin:${PATH}"
-[ -d "${HOME}/bin" ] && PATH="${HOME}/bin:${PATH}"
-[ -d "/Library/Frameworks/Python.framework/Versions/2.7" ] && PYTHONPATH="/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/:${PYTHONPATH}"
-[ -d "/Library/Java/Home" ] && export JAVA_HOME="/Library/Java/Home"
+[ -d "/opt/local/bin" ] 									&& PATH="/opt/local/bin":${PATH}
+[ -d "/opt/local/sbin" ] 									&& PATH="/opt/local/sbin":${PATH}
+[ -d "/usr/local/bin/gitx" ] 								&& PATH="/usr/local/bin/gitx":${PATH}
+[ -d "/Library/Frameworks/Python.framework/Versions/2.7" ] 	&& PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin:${PATH}" #set Python 2.7
+[ -d "${HOME}/Projects/Python/xml2json" ] 					&& PATH="${HOME}/Projects/Python/xml2json:${PATH}"
+[ -d "/usr/local/git" ] 									&& PATH="/usr/local/git/bin:${PATH}"
+[ -d "${HOME}/bin" ] 										&& PATH="${HOME}/bin:${PATH}"
+[ -d "/usr/local/include" ] 								&& PATH="/usr/local/include":${PATH}
+[ -d "/Library/Frameworks/Python.framework/Versions/2.7" ] 	&& PYTHONPATH="/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/:${PYTHONPATH}"
+[ -d "/Library/Java/Home" ] 								&& export JAVA_HOME="/Library/Java/Home"
+[ -d "/opt/local/share/man" ] && export MANPATH="/opt/local/share/man":$MANPATH
 
 export PATH

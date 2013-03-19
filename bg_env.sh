c646aae6c292747e50207f3587aa06433ded534e
Bruno Gama <bgamap@gmail.com>
2013-03-19 15:12:08 -0300

    Removed all the settings for virtualenv and virtualenvwrapper (pythonbrew already have environment settings.)

--- bg_env.sh
+++ bg_env.sh
@@ -36,18 +36,6 @@ export PATH
 # Python
 [ -f "${HOME}/.pythonrc" ] && export PYTHONSTARTUP="${HOME}/.pythonrc"
 
-# virtualenvwrapper
-# export VIRTUALENVWRAPPER_PYTHON=$(which python)
-# export WORKON_HOME=$HOME/.virtualenvs
-# [ -f /usr/local/bin/virtualenvwrapper.sh ]  && . /usr/local/bin/virtualenvwrapper.sh
-# # [ -f /usr/local/share/python/virtualenvwrapper_lazy.sh ]  && source /usr/local/share/python/virtualenvwrapper_lazy.sh
-# # export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--distribute --python=/usr/local/Cellar/python/2.7.3/bin/python'
-# export PIP_RESPECT_VIRTUALENV=true
-
-# # disable PS1 append by the activate hook from virtualenvwrapper
-# export VIRTUAL_ENV_DISABLE_PROMPT=' '
-
-
 # MISC
 export ARCHFLAGS="-arch i386 -arch x86_64"
 export HISTIGNORE="${HISTIGNORE}:&:ls:[bf]g:exit:ls *:cd:cd -:pwd;exit:date:* --help"
